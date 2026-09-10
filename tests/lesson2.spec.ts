4d3fc402b7ba0cb860f382f4b619ff0bae80a82d
mariiabm <maria.b.martines@gmail.com>
2026-09-11 01:24:45 +0300

    lesson2 fix 'execute a task', 'remove a task'

--- tests/lesson2.spec.ts
+++ tests/lesson2.spec.ts
@@ -1,31 +1,38 @@
 import { test, expect } from '@playwright/test';
 
+const TODO_TEXT = 'Buy milk';
+
+test.beforeEach(async ({ page }) => { 
+    
+    await page.goto('https://demo.playwright.dev/todomvc/'); 
+    
+
+})
+
 test('add a new task', async ({ page }) => {
 
-await page.goto('https://demo.playwright.dev/todomvc/');
-await page.locator('input.new-todo').fill('Buy milk');
+await page.locator('input.new-todo').fill(TODO_TEXT);
 await page.locator('input.new-todo').press('Enter');
-await expect(page.getByText('Buy milk')).toBeVisible();
+await expect(page.getByText(TODO_TEXT)).toBeVisible();
 
 });
 
 test('execute a task', async ({ page }) => {
 
-await page.goto('https://demo.playwright.dev/todomvc/');
-await page.locator('input.new-todo').fill('Buy milk');
+await page.locator('input.new-todo').fill(TODO_TEXT);
 await page.locator('input.new-todo').press('Enter');
-await expect(page.getByText('Buy milk')).toBeVisible();
-await page.getByRole('checkbox').check();
-await expect(page.getByText('Buy milk')).toHaveClass(/completed/);
+await expect(page.getByText(TODO_TEXT)).toBeVisible();
+await page.locator('.view').filter({ hasText: TODO_TEXT }).getByRole('checkbox').click();
+await expect(page.locator('.todo-list li').filter({ hasText: TODO_TEXT })).toHaveClass(/completed/);
 
 });
 
 test('remove a task', async ({ page }) => {
 
-await page.goto('https://demo.playwright.dev/todomvc/');
-await page.locator('input.new-todo').fill('Buy milk');
+await page.locator('input.new-todo').fill(TODO_TEXT);
 await page.locator('input.new-todo').press('Enter');
-await expect(page.getByText('Buy milk')).toBeVisible();
-await page.getByRole('button', { name: 'Destroy' }).click();
-await expect(page.getByText('Buy milk')).not.toBeVisible();
+await expect(page.getByText(TODO_TEXT)).toBeVisible();
+await page.getByTestId('todo-item').filter({ hasText: TODO_TEXT }).hover();
+await page.locator('.view').filter({ hasText: TODO_TEXT }).getByRole('button', { name: 'Delete' }).click();
+await expect(page.getByText(TODO_TEXT)).not.toBeVisible();
 });
