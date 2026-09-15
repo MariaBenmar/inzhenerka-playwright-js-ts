d3c991f6d9d41f57ba9fd89f486a929cbf0b737e
mariiabm <maria.b.martines@gmail.com>
2026-09-15 21:07:11 +0300

    add lesson5

---
+++ tests/lesson5.spec.ts
@@ -0,0 +1,41 @@
+import {test, expect} from '@playwright/test';
+
+test.describe('CSS or xPath', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://demo.playwright.dev/todomvc/');
+    });
+    
+    test('use CSS selector :nth-child.', async ({ page }) => {
+        for (let i = 1; i <= 3; i++) {
+            await page.locator('input.new-todo').fill(`Task ${i}`);
+            await page.locator('input.new-todo').press('Enter');
+        }
+
+        await expect(page.locator('.todo-list li:nth-child(2)')).toHaveText('Task 2');
+        
+    });
+
+    test('use CSS + :not()', async ({ page }) => {
+        for (let i = 1; i <= 3; i++) {
+            await page.locator('input.new-todo').fill(`Task ${i}`);
+            await page.locator('input.new-todo').press('Enter');
+        }
+        await page.locator('.todo-list li:nth-child(2) .toggle').click();
+        await page.locator('a[href$="#/completed"]').click();
+        await expect(page.locator('.todo-list li.completed')).toHaveCount(1);   
+        await expect(page.locator('.todo-list li:not(.completed)')).toHaveCount(2);
+    });
+
+    test('use xPath', async ({ page }) => {
+        for (let i = 1; i <= 3; i++) {
+            await page.locator('input.new-todo').fill(`Task ${i}`);
+            await page.locator('input.new-todo').press('Enter');
+        }
+        await page.locator('xpath=//label[text()="Task 1"]/input/ancestor::li').click;
+        await page.locator('li:has(label:text("Task 1"))').click();
+        
+        
+        
+    });
+
+});
