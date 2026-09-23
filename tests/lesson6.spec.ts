704e8826ed8bb0c530fcb0f698f80aa1852f8e06
mariiabm <maria.b.martines@gmail.com>
2026-09-23 16:24:09 +0200

    add fixes try catch in lesson6

--- tests/lesson6.spec.ts
+++ tests/lesson6.spec.ts
@@ -17,14 +17,19 @@ test.describe('errors, trace', () => {
 
     test('trace', async ({ page, context }) => {
         await context.tracing.start({ screenshots: true, snapshots: true });
-        for (let i = 1; i <= 4; i++) {
-             await page.locator('input.new-todo').fill(`Task ${i}`);
-            await page.locator('input.new-todo').press('Enter');
+        try{
+            for (let i = 1; i <= 4; i++) {
+                await page.locator('input.new-todo').fill(`Task ${i}`);
+                await page.locator('input.new-todo').press('Enter');
+           }
+            await page.locator('.todo-list li:nth-child(2) .toggle').click();
+            await expect(page.locator('.todo-list li.completed')).toHaveCount(1);   
+            await expect(page.locator('.todo-list li:not(.completed)')).toHaveCount(2);
+        } 
+        catch (error) {
+        await context.tracing.stop({ path: 'test-results/trace.zip' });
         }
-        await page.locator('.todo-list li:nth-child(2) .toggle').click();
-        await expect(page.locator('.todo-list li.completed')).toHaveCount(1);   
-        await expect(page.locator('.todo-list li:not(.completed)')).toHaveCount(2);
-        await context.tracing.stop({ path: 'trace.zip' });
+
     });
 
 });
